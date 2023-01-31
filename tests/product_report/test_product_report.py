d26d803678974300e952d78dc2da2329a52af943
Fermisson-Gomes <fermison.barbosa@gmail.com>
2023-01-31 14:51:31 -0300

    [feature] requisito 1

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,19 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        id=1,
+        nome_do_produto="Test product",
+        nome_da_empresa="Test company",
+        data_de_fabricacao="2020-10-20",
+        data_de_validade="2021-10-20",
+        numero_de_serie=12345,
+        instrucoes_de_armazenamento="Instructions"
+    )
+
+    assert str(product.__repr__()) == f"O produto {product.nome_do_produto}\
+ fabricado em {product.data_de_fabricacao} por\
+ {product.nome_da_empresa} com validade até\
+ {product.data_de_validade} precisa ser armazenado\
+ {product.instrucoes_de_armazenamento}."
